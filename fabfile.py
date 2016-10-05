66616b14d8b7090b372931c9dae97f89ce885297
ultraturtle0 <jordankusel@my.unt.edu>
2016-10-05 23:28:21 +0000

    fabric script edit

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,52 @@
-from fabric.api import abort, cd, lcd, local, run, settings
+from fabric.api import abort, cd, env, lcd, local, prefix, run, settings
 from fabric.contrib.console import confirm
+from contextlib import contextmanager as _contextmanager
+
+projname = 'kusel'
+dirname = 'website'
+envname = 'site'
+
+env.directory = '/home/pi/python/{}'.format(dirname)
+env.activate = 'source /home/pi/python/{}/{}/bin/activate'.format(dirname, envname)
+env.colorize_errors = True
+
+apps = [
+    'blog',
+]
+
+local_shell = '/bin/bash'
+
+@_contextmanager
+def virtualenv():
+    with cd(env.directory):
+        with prefix(env.activate):
+            yield
 
 def test():
     with settings(warn_only=True):
-        result = local("./manage.py test blog", capture=True)
-    if result.failed and not confirm("Tests failed. Continue anyway?"):
-        abort("Aborting at user request.")
+        for a in apps:
+            app_test = "./manage.py test {}".format(a)
+            result = local(app_test, capture=True)
+            if result.failed and not confirm("'{}' tests failed. Continue?".format(a)):
+                abort("Aborting at user request.")
 
 def commit():
-    local("git add -p && git commit")
+    local("git add -p && git commit", shell=local_shell)
 
 def push():
-    local("git push")
+    local("git push", shell=local_shell)
 
 def prepare_deployment(branch_name):
-    test()
-    commit()
-    push()
+    with virtualenv():
+        with lcd(projname):
+            test()
+            commit()
+            push()
 
 def deploy():
-    code_dir = '/home/pi/python/website/kusel/'
-    with cd(code_dir):
-        run('git pull')
-        run('python manage.py migrate blog')
-        run('python manage.py runserver')
+    with virtualenv():
+        with lcd(projname):
+            run('git pull')
+            for a in apps:
+                run('./manage.py migrate {}'.format(a))
+            run('./manage.py runserver')
