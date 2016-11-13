804b00b7dbc15d77aa05fc0ba0c13f73b0a68d26
ultraturtle0 <jordankusel@my.unt.edu>
2016-11-13 11:43:05 +0000

    linen file refactor and bio addition

--- linen/config.py
+++ linen/config.py
@@ -1,3 +1,18 @@
+REPO_URL = 'https://github.com/ultraturtle0/website.git'
+REPO_BRANCH = 'deployment'
+
+PYTHON = 'python3.4'
+VIRTUALENV = 'virtualenv'
+LOCALENV = 'site'
+
+APACHE_HOST = '138.197.20.29'
+MYSQL_HOST = '138.197.20.29'
+MEDIA_HOST = '138.197.20.29'
+
+DOMAIN_NAME = 'jordankusel.xyz'
+
+APACHE_LOGLEVEL = 'warn'
+
 SUPERUSER = {
     'username': 'ultraturtle0',
     'email': 'jordankusel@my.unt.edu',
