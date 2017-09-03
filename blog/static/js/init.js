f82a17170be6fe94caf7420c21060295484b25f9
Jordan Kusel <jordankusel@my.unt.edu>
2017-09-03 01:17:31 -0700

    moved transitions to separate module

--- blog/static/js/init.js
+++ blog/static/js/init.js
@@ -1,3 +1,23 @@
+console.log(window.transitions !== false);
+
 $(document).ready(function () {
     init();
 });
+
+var init = function() { 
+    $('a').each(function (index) { 
+        $(this).hover(transitions.textFocusOn, transitions.textFocusOff); 
+    }); 
+ 
+    $('h1') 
+        .css({left: '-30'}) 
+        .animate({opacity: 1.0, left: '0'}, 
+            'slow', 
+            'swing', 
+        ); 
+ 
+    setTimeout(function () { 
+        $('a').each(transitions.textSlide); 
+    }, 200); 
+ 
+};
--- blog/static/js/transitions.module.js
+++ blog/static/js/transitions.module.js
@@ -1,41 +1,26 @@
-$(document).ready(function () {
-    init();
-});
+var transitions = (function () {
 
-var init = function() {
-    $('a').each(function (index) {
-        $(this).hover(textFocusOn, textFocusOff);
-    });
+    window.transitions = window.transitions || {};
 
-    $('h1')
-        .css({left: '-30'})
-        .animate({opacity: 1.0, left: '0'},
-            'slow',
-            'swing',
-        );
+    transitions.textFocusOn = function () {
+        $(this).animate({opacity: 1.0}, 'fast');
+    };
 
-    setTimeout(function () {
-        $('a').each(textSlide);
-    }, 200);
+    transitions.textFocusOff = function () {
+        $(this).animate({opacity: 0.6}, 'fast');
+    };
 
-};
+    transitions.textSlide = function(index) {
+        var self = this;
+        setTimeout(function () {
+            $(self)
+                .css({opacity: 0.0, left: '50'})
+                .animate({opacity: 0.6, left: '0'},
+                    'slow',
+                    'swing');
+        }, index * 100);
+    };
 
-var textFocusOn = function () {
-    $(this).animate({opacity: 1.0}, 'fast');
-};
-
-var textFocusOff = function () {
-    $(this).animate({opacity: 0.6}, 'fast');
-};
-
-var textSlide = function(index) {
-    var self = this;
-    setTimeout(function () {
-        $(self)
-            .css({opacity: 0.0, left: '50'})
-            .animate({opacity: 0.6, left: '0'},
-                'slow',
-                'swing');
-    }, index * 100);
-};
+    return transitions;
+})();
 
