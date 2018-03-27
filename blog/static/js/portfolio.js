ce606e11478c52a8cb1fcf819af4978c0b329db7
Jordan Kusel <jordankusel@my.unt.edu>
2018-03-26 18:11:44 -0700

    old sass and html work

--- blog/static/js/portfolio.js
+++ blog/static/js/portfolio.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     
-    $('h3').each(function (index) {
+    $('li').each(function (index) {
         $(this)
-            .attr('opacity', 0.7)
-            //.attr('child', 'a')
+            .attr('opacity', 0.6)
+            .attr('child', 'a')
             .hover(transitions.textFocusOn, transitions.textFocusOff);
     });
 });
